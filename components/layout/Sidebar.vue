04cde6a59068eb7f4b9de83d8b4a5f34e308d896
8thgencore <axelmaker@ya.ru>
2024-12-18 22:39:16 +0300

    ✨ Add Tailwind

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -7,5 +7,8 @@
         <NuxtLink to="/" class="mb-10 block">
             <NuxtImg src="/logo.svg" alt="Logo" width="100px" class="mx-auto" />
         </NuxtLink>
+        <button class="absolute top-2 right-3 transition-colors hover:text-primary">
+            <Icon name="line-md:logout" size="20" />
+        </button>
     </aside>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,25 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: false },
-  modules: ["@nuxt/image", "@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  modules: [
+    "@nuxt/image",
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@nuxt/icon",
+    [
+      "@nuxtjs/google-fonts",
+      {
+        families: {
+          Lato: {
+            wght: [300, 400, 700],
+            ital: [300],
+          },
+        },
+      },
+    ],
+  ],
   shadcn: {
-    prefix: "",
+    prefix: "UI",
     componentDir: "./components/ui",
   },
 });
