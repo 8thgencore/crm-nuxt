b2366111c34349f23c20a87d5c3df01a57fee704
8thgencore <axelmaker@ya.ru>
2024-12-21 16:27:13 +0300

    ✨ Add login page

---
+++ pages/login.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+useSeoMeta({
+    title: "Login | CRM System",
+});
+
+const emailRef = ref("");
+const passwordRef = ref("");
+const nameRef = ref("");
+
+
+</script>
+
+<template>
+    <div class="flex justify-center items-center min-h-screen w-full">
+        <div class="rounded bg-sidebar w-1/4 p-5">
+            <h1 class="text-2xl font-bold text-center mb-5">Login</h1>
+
+            <form>
+                <UIInput v-model="emailRef" placeholder="Email" type="email" class="mb-3" />
+                <UIInput v-model="passwordRef" placeholder="Password" type="password" class="mb-3" />
+                <UIInput v-model="nameRef" placeholder="Name" type="text" class="mb-3" />
+                <div class="flex items-center justify-center gap-5">
+                    <UIButton type="button">Login</UIButton>
+                    <UIButton type="button">Register</UIButton>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
